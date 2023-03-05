a03cc725577089e49484f175db2a84bca8f080eb
DHruV PaNDIT <96495714+DhRV00T@users.noreply.github.com>
2023-03-05 23:32:36 +0530

    DroidX:Violet Changelogs

---
+++ Violet.mk
@@ -0,0 +1,14 @@
+==================
+Device Changelogs
+==================
+
+---------------------
+Date: March 06,2023
+---------------------
+
+- Initial official GMS build
+- Firmware included by default
+- Nuked out moto Dolby
+- Fully rebase tree
+- Set full gesture navigation by default
+- Some misc changes
