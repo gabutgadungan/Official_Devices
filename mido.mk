5df0b84ce380d834bd1767e6a9da29a670786dde
DHruV PaNDIT <96495714+DhRV00T@users.noreply.github.com>
2023-03-05 15:16:44 +0530

    DroidX: (Alpha One) Mido Changelogs

---
+++ mido.mk
@@ -0,0 +1,9 @@
+               =================
+               Device Changelogs
+               =================
+====================
+Date: March 5,2023
+====================
+- Initial Official Release.
+- Upstreamed kernel to latest.
+- Some minor bug fixes.
