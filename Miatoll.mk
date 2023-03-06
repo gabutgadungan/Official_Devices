9a0336c50cdb9d0f93dd92dc766c712662b7220f
DHruV PaNDIT <96495714+DhRV00T@users.noreply.github.com>
2023-03-06 23:09:08 +0530

    DroidXUI: miatoll changelogs v1.0

---
+++ Miatoll.mk
@@ -0,0 +1,10 @@
+==================
+Device Changelogs
+==================
+
+---------------------
+Date: March 06,2023
+---------------------
+
+- Initial official release.
+- Recommended recovery https://t.me/DroidXUI_chats/2432
