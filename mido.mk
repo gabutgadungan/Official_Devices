7dcd2efe6952ff1fcece1d460518745582835b10
NRanjan-17 <nalinishranjan05@gmail.com>
2023-03-21 23:39:43 +0530

    official_devices: ALPHA ONE changelog
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- mido.mk
+++ mido.mk
@@ -1,9 +1,17 @@
-               =================
-               Device Changelogs
-               =================
+=================
+Device Changelogs
+=================
+
 ====================
-Date: March 5,2023
+ALPHA ONE
+====================
+Date: March 21,2023
 ====================
 - Initial Official Release.
 - Upstreamed kernel to latest.
-- Some minor bug fixes.
+- All Stuff Working
+- Clean Flash Recommended
+- BY-DEFAULT Encrypted Rom
+- Only Vanilla for now
+- Recovery: https://github.com/zeelog/device_mido_twrp/releases/tag/2.0
+- Guide: https://raw.githubusercontent.com/NRanjan-17/Pixel-Experience-Releases/main/install_guide.md
