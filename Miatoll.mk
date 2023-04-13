c0843d23a1c5a2bc9813d2bc0e899002151ba7b4
DHruV PaNDIT <96495714+DhRV00T@users.noreply.github.com>
2023-04-13 09:41:34 +0530

    Miatoll: v1.2 changelogs

--- Miatoll.mk
+++ Miatoll.mk
@@ -3,6 +3,31 @@ Device Changelogs
 ==================
 
 ---------------------
+Date: April 13,2023
+---------------------
+• Switched to common Xiaomi light AIDL
+• Switched to common Xiaomi fingerprint HIDL
+• Switched to AIDL power hal
+• Removed aptX(HD) encoder shared libs
+• Don't inherit updatable_apex.mk
+• Build QTI vibrator from source
+• Updated USB controller configuration
+• Imported excluded-input-devices.xml
+• Removed AUDIO_FEATURE_ENABLED_PROXY_DEVICE flag
+• Flatten APEXs for performance
+• Inherit several Android Go configurations
+• Update media codecs from V13.0.2.0.SJZMIXM
+• Disabled ART debug and optimized dexpreopt
+• Decommanized Tree
+• Merged latest clo tag 'LA.UM.9.1.r1-13400-SMxxx0.QSSI12.0' into kernel
+• Merged latest tag 'v1.0.20220627' of net/wireguard into kernel
+• Reduced/Killed/Silenced alot of log spam in kernel
+• Increased QS panel smoothness after QPR2 update
+• Overall increased Stability & smoothness
+
+
+
+---------------------
 Date: March 06,2023
 ---------------------
 
