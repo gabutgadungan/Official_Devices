57c9b5319b62e9891a03a11f74c0cb3ee3c43737
DHruV PaNDIT <96495714+DhRV00T@users.noreply.github.com>
2023-04-18 21:03:53 +0530

    alioth: V1.2 DXUI changelogs

--- alioth.mk
+++ alioth.mk
@@ -1,6 +1,13 @@
 ===================
 Device changelogs
 ===================
+Date: April 18,2023
+===================
+- Updated build fp to 14.0.4.0
+- switch refresh rate 2 times to use 120hz
+- To open wallpaper and style from settings instead of launcher
+
+
 Date: March 06,2023
 ===================
 - Initial build.
