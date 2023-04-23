9ae9b1a8de2692f26c4364ea010964380a21e9b0
Noobie_ashwani <99090768+ashwani212@users.noreply.github.com>
2023-04-23 17:24:31 +0530

    mido: v1.2 changelogs

--- mido.mk
+++ mido.mk
@@ -1,6 +1,29 @@
-=================
+====================
 Device Changelogs
-=================
+====================
+Notes:
+====================
+- Only Dirty flash if you are coming from previous droidx official encrypted build if coming from unofficial or other builds better do a clean flash
+- FBE (File based encryption) enabled by default *
+- NON - RETROFIT BUILD. Flash or Update as usual, with normal method.
+- * use updated TWRP (https://github.com/zeelog/device_mido_twrp/releases/tag/2.0)
+- Use zeelog's lineageos method to flash the rom or follow the following steps given here: [Guide](https://raw.githubusercontent.com/NRanjan-17/Pixel-Experience-Releases/main/install_guide.md)
+- Clean Flash Recommended
+- Always dirty flash at your own risk.
+- If your device bootloops, revert to an older build that boots, backup your stuff and do a clean flash.
+
+====================
+ALPHA TWO
+====================
+Date: April 23,2023
+====================
+- April 2023 Release
+- Non retrofit build
+- mido: Enable LTE_CA workaround
+- mido: Inherit 2048 dalvik config as fallback for GSIs
+- mido: props: Enable Seamless Transfer support
+- mido: enable GL comp backpressure
+- mido: Increase 5 GHz network signal tolerance
 
 ====================
 ALPHA ONE
