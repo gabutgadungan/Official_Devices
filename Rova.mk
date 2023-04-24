e3a48746c1c47ffb6f9c51b446db05fcb8d64058
DHruV PaNDIT <96495714+DhRV00T@users.noreply.github.com>
2023-04-24 18:50:31 +0530

    rova: v1.2 Changelogs

--- Rova.mk
+++ Rova.mk
@@ -2,11 +2,43 @@
 Device Changelogs (v1.2)
 ========================
 
-------------------------
-Date: April 13,2023
-------------------------
+========================
+FLASHING INSTRUCTIONS 
+========================
+Flashing instructions:
+First time installers:
+1. Enter recovery (Wipe dalvik, system, vendor , data and cache) 
+2. Flash the rom 
+3. Reboot and wait sometime for first boot 
+4. Now reboot back to recovery and dirty flash the rom 
+5. Flash Gapps and magisk (optional)
+5. wipe dalvik/cache 
+6. Reboot and enjoy😉
+NOTE:
+Follow the instruction properly to avoid freezing Issues
 
-- Initial release
-- Increased HW_TIMEOUT_MULTIPLIER to workaround ANRs due to  Magisk 
-- Added restart SystemUI to Advanced Reboot- Shipped with "4.9.337-3.10-iusmac" kernel
-- Optimized graphics render engine - Some minor fixes and improvements.
+UPDATE:
+1. Enter recovery  
+2. Flash the update 
+3. Flash Gapps and magisk (optional) 
+4. wipe dalvik/cache  
+5. Reboot and enjoy😉
+
+NOTE:
+- Reflashing gapps again after flashing the new update mandatory for working of google apps
+
+
+------------------------
+Date: April 24,2023
+------------------------      
+- Initial release 
+- Added toggle to force enable LTE CA
+- Added moto Audio-Dolby  (thx to itorenz)  
+- Shipped with v4.9.337-msm8937 kernel 
+- Some minor fixes and improvements
+
+know issues:
+- Display option in setting crashes
+
+Notes: 
+- If anyone face jitter in launcher and recent change to pixel launcher
