2c706426fcb73b48c5f9971eda2f4d53e3afe121
Noobie_ashwani <99090768+ashwani212@users.noreply.github.com>
2023-05-18 17:11:16 +0530

    RMX1851: v1.3 changelogs

--- rmx1851.mk
+++ rmx1851.mk
@@ -1,6 +1,11 @@
 ==================
 Device Changelogs
 ==================
+Date: May 18,2023
+------------------
+- some bugs fixes and improvements in trees.
+
+
 
 ---------------------
 Date: March 08,2023
