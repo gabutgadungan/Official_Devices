35144e2fe97b9119ca37446ece7a014fd98c1896
Noobie_ashwani <99090768+ashwani212@users.noreply.github.com>
2023-05-20 16:34:37 +0530

    d1: v1.3 changelogs

---
+++ d1.mk
@@ -0,0 +1,8 @@
+=========================
+Device Changelogs || DXUI
+=========================
+Date: 19-May-2023
+-------------------------
+- some bugfixes.
+- upstreamed kernel.
+- some miscellaneous changes.
