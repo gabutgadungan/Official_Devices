9ab70b3494597fd9b76d908422d866f76a2861d0
janak <niraulajanak2019@gmail.com>
2023-05-25 17:13:05 +0000

    mi439: v1.3 changelogs

--- mi439.mk
+++ mi439.mk
@@ -1,18 +1,17 @@
 CHANGELOG (DEVICE):
 
-1. Comes with April Security Patch.
-2. Added LTE CA Force toggle.
+1. Comes with May Security Patch.
 3. Topaz kernel as default.
 4. The tree has been replaced (MI439-CLO) by Jprimero.
 
 Notes:
-1. Comes with vanilla build only.
+1. Comes with Gapps build only.
 2. Clean flash recommended.
 4. Initial build, expect features missing and minor bugs.
 
 SOURCE CHANGELOG:
 
-Date: 29-April-2023
+Date: 25-May-2023
 -------------------
 
-- Initial official release for Alpha Two.
+- Initial official release for Beta one.
