38f417578d55a3851cd7c44cc4d574b97beb87d9
DHruV PaNDIT <96495714+DhRV00T@users.noreply.github.com>
2023-07-22 23:32:38 +0530

    devon: v1.5 changelog (20230722)

--- devon.mk
+++ devon.mk
@@ -3,6 +3,14 @@ Device Changelogs
 ===================
 Maintainer: Dhruv00
 ===================
+Date: 22-July-2023
+===================
+- Fixed in call volume stuck on loudspeaker.
+- Fixed call recording bug.
+- Added support for dynamic refresh rate.
+
+
+===================
 Date: 12-June-2023
 ===================
 - initial official release.
