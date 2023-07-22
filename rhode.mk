44d576b825bfe969f34dcae3f11fb832ecbf52cc
DHruV PaNDIT <96495714+DhRV00T@users.noreply.github.com>
2023-07-22 23:34:12 +0530

    rhode: v1.5 changelog (20230722)

--- rhode.mk
+++ rhode.mk
@@ -3,6 +3,12 @@ Device Changelogs
 ===================
 Maintainer: Dhruv00
 ===================
+- Added support for dynamic refresh rate.
+- Fixed in call volume stuck at loudspeaker.
+- Fixed call recording bug.
+
+
+===================
 Date: 12-June-2023
 ===================
 - Initial Official release.
