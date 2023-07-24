570dd7b31fe6fc3204b2e8eaa5b73b240859bb7c
Arijit78 <sahaarijit2007@gmail.com>
2023-07-24 05:42:05 +0200

    whyred: Initial 1.5 official release

---
+++ whyred.mk
@@ -0,0 +1,10 @@
+==================
+Device: Redmi Note 5 Pro (whyred)
+Maintainer: ARIJIT-SAHA
+==================
+Device Changelogs
+==================
+------------------
+Date: July 24,2023
+------------------
+- initial official release.
