c08186fbdbb30d0abfe7fe8305d0b18fb192034a
chaitanya <88253340+Chaitanyakm@users.noreply.github.com>
2023-07-24 13:24:12 +0530

    Update alioth.mk

--- alioth.mk
+++ alioth.mk
@@ -1,12 +1,22 @@
 ===================
 Device changelogs
 ===================
+Date: July 24,2023
+===================
+- Included Basic Call recorder
+- Updated blobs from 14.0.7.0 global
+- Included leica camera 4.9
+- redline kernel compiled with aosp clang 17
+- more under the hood changes and improvments
+
+===================
+Device changelogs
+===================
 Date: April 18,2023
 ===================
 - Updated build fp to 14.0.4.0
 - switch refresh rate 2 times to use 120hz
 - To open wallpaper and style from settings instead of launcher
-
 
 Date: March 06,2023
 ===================
