b440e97c0591aede651cb04d996551138e4f048f
Thunder105 <hariprakashmurugesan@gmail.com>
2023-07-26 13:51:24 +0530

    spartan: update to v1.5

---
+++ spartan.mk
@@ -0,0 +1,12 @@
+==================
+Device Changelogs
+==================
+Date: July 26,2023
+------------------
+
+- Initial Official Release
+- Both Vanilla and Gapps build available
+- Selinux is Enforcing
+- Addded Minazuki Kernel
+- and misc changes
+
