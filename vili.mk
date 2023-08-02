eb5e06afe41fc28241e47c6847699eacbc584156
Noobie_ashwani <99090768+ashwani212@users.noreply.github.com>
2023-08-02 10:14:49 +0530

    vili: v1.5 official release

---
+++ vili.mk
@@ -0,0 +1,18 @@
+=================
+Device changelogs
+=================
+Date: 02/08/2023
+
+• Shipped with ReBorn
+• Merged with July Security Update 
+• KernelSU Updated
+• Display blobs from Sony Xperia 1 III
+• HDR Blobs from tundra
+• OP Dolby Atmos Added 
+• WoWiFi/WiFi Calling Fixed
+• Netflix HDR fixed
+• QS Panel Lag Fixed
+• USB not recognising issue Fixed
+• Update Perf HAL to 2.3
+• Update HotwordEnrollment blobs (Enhanced Voice Recognition in Assistant)
+• Enable Multi-Gen LRU
