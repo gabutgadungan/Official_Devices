c5af668c5b8530485a758985d0e1c65a761411d2
DHruV PaNDIT <96495714+DhRV00T@users.noreply.github.com>
2023-08-11 15:36:39 +0530

    hawao: v1.5.1 update

--- hawao.mk
+++ hawao.mk
@@ -3,6 +3,12 @@ Device Changelogs
 ===================
 Maintainer: Dhruv00
 ===================
+Date: 11-Aug-2023
+===================
+- some minor fixes.
+
+
+===================
 Date: 12-June-2023
 ===================
 - initial official release.
