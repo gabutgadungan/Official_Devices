296ba67e6d3b7562cf290dd4dabe049bc6b2b9d8
DHruV PaNDIT <96495714+DhRV00T@users.noreply.github.com>
2023-08-11 13:49:14 +0530

    devon: v1.5.1 changelogs

--- devon.mk
+++ devon.mk
@@ -3,6 +3,12 @@ Device Changelogs
 ===================
 Maintainer: Dhruv00
 ===================
+Date: 11-August-2023
+===================
+- some minor fixes.
+
+
+===================
 Date: 22-July-2023
 ===================
 - Fixed in call volume stuck on loudspeaker.
