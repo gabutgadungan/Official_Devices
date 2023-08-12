264206cd4574dcd4696753ded3c3e95d1d6c128d
Thunder105 <hariprakashmurugesan@gmail.com>
2023-08-12 22:42:19 +0530

    spartan: 1.5.1 Changelog

--- spartan.mk
+++ spartan.mk
@@ -1,4 +1,15 @@
 ==================
+Device changelogs
+==================
+Date: 12/08/2023
+==================
+
+- Fixed brightness flicker issue in lockscreen
+- Fixed Launcher keeps stopping issue(source)
+- CTS passes with default kernel
+- Misc changes
+
+
 Device Changelogs
 ==================
 Date: July 26,2023
