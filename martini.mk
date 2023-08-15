ab21cdba8ff7704f971107a5c10df48ff1e8285a
Mrick343 <mrinmoy8429de@gmail.com>
2023-08-15 21:14:48 +0530

    martini: v1.5.1 changelog

--- martini.mk
+++ martini.mk
@@ -1,7 +1,21 @@
 ===================
 Device Changelogs
 ===================
+Date: 15-August-2023
+-------------------
+
+- Added Livedisplay
+- Updated blobs from OOS 13.1.0.590(EX01)
+- Adreno update to v@600
+- Added OOS CAM
+- Kernel Updated to 5.4.259 (stable release)
+- Drop useless camera configs
+
+===================
+Device Changelogs
+===================
 Date: 22-April-2023
 -------------------
 
 - Initial official release.
+
