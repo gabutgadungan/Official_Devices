abc438fed01cccbbb10918fea53efbae2648f80f
Mrinmoy Dey <mrinmoy8429de@gmail.com>
2023-08-18 22:49:09 +0530

    Update martini.mk

--- martini.mk
+++ martini.mk
@@ -8,7 +8,7 @@ Date: 15-August-2023
 - Updated blobs from OOS 13.1.0.590(EX01)
 - Adreno update to v@600
 - Added OOS CAM
-- Kernel Updated to 5.4.259 (stable release)
+- Kernel Updated to 5.4.249 (stable release)
 - Drop useless camera configs
 
 ===================
