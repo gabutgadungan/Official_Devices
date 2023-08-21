b11dd1b8817c7377492ecfdb130e20f63ff7a9b2
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-08-21 22:08:16 +0700

    X01AD: Stable build DroidX-UI Official

--- X01AD.mk
+++ X01AD.mk
@@ -1,6 +1,15 @@
 ===================
 Device changelogs
 ===================
+Date: August 21,2023
+===================
+- Stable build
+- Fixed radio fm
+- Update GPS from LA.UM.9.6.3.r1-06200-89xx.0
+- and more improvement
+
+
+==================
 Date: August 20,2023
 ===================
 - Initial Build
