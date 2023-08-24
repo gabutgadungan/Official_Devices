aef53ab1813f2769ffd5d67050423e27a10f3915
ZirgomHaidar <zirgomhaidar17@gmail.com>
2023-08-24 11:25:30 +0530

    chime: 1.5.1 Changelogs

---
+++ citrus.mk
@@ -0,0 +1,9 @@
+===================
+Device changelogs
+===================
+Date: August 24,2023
+===================
+- Initial Release 
+- Moved With Lilium Kernel
+- Some Bug Fixes.
+- Miscellaneous Changes
