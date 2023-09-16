40eee469a092e57df47a0fd24cfbbc3d4e834eb9
INCENDIOCODM <vinayakgamemaster42@gmail.com>
2023-09-16 19:38:41 +0530

    haydn: update to limedust 1.5.2

--- haydn.mk
+++ haydn.mk
@@ -1,9 +1,20 @@
+==================
+Device Changelogs
+==================
+Date: 16-09-2023
+------------------
+- KernelSU Updated to 0.6.7
+- August Security Patch
+- Synced the latest source
+- Fixed NFC for CN devices
+- Updated leica cam to 5.0
+
 =========================
 Device Changelogs || DXUI
 =========================
 Date: 15-08-2023
 -------------------------
-- Initial Build 
+- Initial Build
 - August 2023 Security Patch
 -Updated vendor blobs from MIUI V14.0.6.0.TKKMIXM
 - Updated firmware from MIUI V14.0.6.0.TKKMIXM
